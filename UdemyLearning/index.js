dafa26547e223b81acc30c8ac4ba948ec59445db
Stefan Misik <stefan.misik@sap.com>
2020-01-05 17:24:53 +0100

    javascript #3 operators

--- UdemyLearning/index.js
+++ UdemyLearning/index.js
@@ -4,3 +4,12 @@ console.log("Hello, from JS console, from external JS file.");
 // Lesson #2: Variables and Datatypes
 var number = 10;
 console.log(number);
+
+// Lesson #3: Operators
+var moduloResult = 3 % 9;
+console.log(moduloResult);
+
+console.log(number += 5);
+
+var result = (number < 100) ? 'yes' : 'no';
+console.log(result);
